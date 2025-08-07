3920263e02332a708f3fcd1227f6e419ef5c6ab2
Sateesh2001 <sateeshtatigiri@gmail.com>
2025-08-07 17:52:23 +0530

    Add files via upload

---
+++ Untitled Notebook 2025-07-18 13_56_00.py
@@ -0,0 +1,14 @@
+# Databricks notebook source
+print('sateesh')
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE TABLE Employees (
+# MAGIC     id INT,
+# MAGIC     name VARCHAR(20),
+# MAGIC     salary INT
+# MAGIC );
+
+# COMMAND ----------
+
